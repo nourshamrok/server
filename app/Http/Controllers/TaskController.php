0a6a2a5c94a91ab333e5379ad33f65864def64c6
Nour Shamrok <nourshaamrok@gmail.com>
2024-02-07 15:42:29 +0200

    first

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+
+        // Search for tasks by title or description
+        $tasks = Task::where('title', 'like', "%$query%")
+                    ->orWhere('description', 'like', "%$query%")
+                    ->get();
+
+        return response()->json($tasks);
+    }
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $task->update($request->all());
+        return response()->json($task, 200);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
